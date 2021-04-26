653838535245e576acdbac8f24c8a04d719c8ca7
admin accenture <IES.DevOps.IT.CAPHolding@accenture.com>
2021-04-26 23:22:56 +0200

    aggiunta

---
+++ Step3/ecr-variables.tf
@@ -0,0 +1,15 @@
+variable "ecr-name" {
+  default = "default"
+}
+variable "ecr-mutability" {
+  default = "IMMUTABLE"
+}
+variable "scan-push" {
+  default = "push"
+}
+variable "tags" {
+  default = "terraform-provisioned"
+}
+variable "repourl" {
+  default = "aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName"
+}
---
+++ Step3/ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "ECRterraform" {
+  name                 = "${var.ecr-name}"
+  image_tag_mutability = "${var.ecr-mutability}"
+  tags                 =  "${var.tags}"
+  repository_url       = "${var.repourl}"
+  #lifecycle_policy
+  image_scanning_configuration {
+    scan_on_push = "${var.scan-push}"
+  }
+}
