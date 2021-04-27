ce2d2e4c3c498556c08657a5add8bdd0e74c4034
cavamagie <9657325+cavamagie@users.noreply.github.com>
2021-04-27 12:32:16 +0200

    Update ecr.tf

--- Step3/ecr.tf
+++ Step3/ecr.tf
@@ -1,8 +1,8 @@
 resource "aws_ecr_repository" "ECRterraform" {
   name                 = "${var.ecr-name}"
   image_tag_mutability = "${var.ecr-mutability}"
-  tags                 =  "${var.tags}"
-  repository_url       = "${var.repourl}"
+  #tags                 =  "${var.tags}"
+  #repository_url       = "${var.repourl}"
   #lifecycle_policy
   image_scanning_configuration {
     scan_on_push = "${var.scan-push}"
